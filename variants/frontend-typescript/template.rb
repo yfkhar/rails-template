e5112a5bb4eed17a264dd1728bf61e1eec4f9684
Gareth Jones <jones258@gmail.com>
2022-03-04 11:45:15 +1300

    feat: setup testing library for typescript too

--- variants/frontend-typescript/template.rb
+++ variants/frontend-typescript/template.rb
@@ -19,7 +19,13 @@ types_packages = %w[
 
 run "yarn remove prop-types"
 yarn_add_dependencies types_packages
-yarn_add_dev_dependencies %w[@typescript-eslint/parser @typescript-eslint/eslint-plugin]
+yarn_add_dev_dependencies %w[
+  @typescript-eslint/parser
+  @typescript-eslint/eslint-plugin
+  @jest/types
+  ts-jest
+  ts-node
+]
 run "yarn install"
 
 %w[
@@ -33,6 +39,9 @@ end
 copy_file "tsconfig.json", force: true
 copy_file ".eslintrc.js", force: true
 copy_file "types.d.ts", force: true
+
+remove_file "jest.config.js"
+copy_file "jest.config.ts"
 
 gsub_file(
   "app/frontend/packs/application.ts",
@@ -69,10 +78,7 @@ File.write("./package.json", JSON.generate(package_json))
 
 # example files
 remove_file "app/frontend/components/HelloWorld.jsx", force: true
-copy_file "app/frontend/components/home/index.tsx", force: true
-copy_file "app/frontend/components/utils/index.ts", force: true
-copy_file "app/frontend/components/utils/Form.tsx", force: true
-copy_file "app/frontend/components/utils/TextInput.tsx", force: true
+copy_file "app/frontend/components/HelloWorld.tsx", force: true
 gsub_file(
   "app/views/home/index.html.erb",
   'react_component("HelloWorld", { greeting: "Hello from react-rails." })',
@@ -90,3 +96,6 @@ append_to_file "bin/ci-run" do
     yarn run typecheck
   TYPECHECK
 end
+
+remove_dir "app/frontend/test"
+directory "app/frontend/test"
